28cae383d8d8c63cc6772146300d4a4da31972ea
ap- <github@ameso.com.au>
2012-02-19 22:59:17 +1100

    add meta tag author

--- header.php
+++ header.php
@@ -5,5 +5,6 @@
  <title>Organic</title>
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <meta name="description" content="">
+ <meta name="author" content="">
 </head>
 <body>
