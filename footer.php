cbfa529866dde483186e068d5ee17c4866bd5d87
ap- <github@ameso.com.au>
2012-02-22 00:35:09 +1100

    simplify footer copyright dynamic name

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
   <footer role="contentinfo">
-      <small><p>© 2012 <?php echo esc_html( get_site_option( 'site_name' ) ); ?></p></small>
+      <small><p>© 2012 <?php echo get_site_option('site_name');?></p></small>
   </footer>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
   <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
