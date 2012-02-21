5e4541130a9f6f71cf8065e446745251c34d7783
ap- <github@ameso.com.au>
2012-02-22 01:00:01 +1100

    add nav with navigation role

--- header.php
+++ header.php
@@ -10,5 +10,32 @@
  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body>
- <header role="banner" style="display:none"><h1><?php echo get_site_option('site_name');?></h1></header>
- <nav role="navigation"></nav>
+ <header role="banner" style="display:none">
+  <h1><?php echo get_site_option('site_name');?></h1>
+ </header>
+ <nav role="navigation">
+  <div class="navbar navbar-fixed-top">
+   <div class="navbar-inner">
+    <div class="container-fluid">
+     <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+     </a>
+     <a class="brand" href="#"><?php echo get_site_option('site_name');?></a>
+     <div class="nav-collapse">
+      <ul class="nav">
+       <li class="active"><a href="#home">Home</a></li>
+       <li><a href="#about">About</a></li>
+       <li><a href="#contact">Contact</a></li>
+      </ul>
+      <p class="navbar-text pull-right">
+       <?php if ( is_user_logged_in() ) 
+        {global $current_user;get_currentuserinfo();echo '<a href="/wp-admin/">' . $current_user->display_name . '</a>';}
+       ?>
+      </p>
+     </div>
+    </div>
+   </div>
+  </div>
+ </nav>
