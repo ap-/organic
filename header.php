3b5d342cc44d317b145b2c47f12cf69f2d31bf3c
ap- <github@ameso.com.au>
2012-02-22 00:08:29 +1100

    add conditional script tag for <ie9 to html5shim via google

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
  <meta name="description" content="">
  <meta name="author" content="">
  <link href="<?php echo get_bloginfo('template_directory'); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+ <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body>
  <header role="banner"></header>
