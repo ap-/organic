26326e70bceea6b29755a2e9ff74004cbd0885ce
ap- <github@ameso.com.au>
2012-02-21 23:22:36 +1100

    add script tag for jquery via google ajax

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
   <footer role="contentinfo">
       <small><p>&copy; Organic 2012</p></small>
   </footer>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 </body>
 </html>
