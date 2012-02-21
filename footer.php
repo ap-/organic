a9e8c656c3a0977be20e2fdf4f012da8222eb83d
ap- <github@ameso.com.au>
2012-02-22 09:06:27 +1100

    clean footer

--- footer.php
+++ footer.php
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 <hr>
 <footer role="contentinfo">
  <small><p>© 2012 <?php echo get_site_option('site_name');?></p></small>
 </footer>
 </div><!--/container-->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
-=======
- <hr>
- <footer role="contentinfo">
-  <small><p>© 2012 <?php echo get_site_option('site_name');?></p></small>
- </footer>
-</div><!--/container-fluid-->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
->>>>>>> separate navigation into own file
 </body>
 </html>
