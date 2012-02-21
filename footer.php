392033e7a3f9f45f2fac18552334429122fb28bb
ap- <github@ameso.com.au>
2012-02-21 23:58:32 +1100

    add script tag for bootstrap

--- footer.php
+++ footer.php
@@ -2,5 +2,6 @@
       <small><p>&copy; Organic 2012</p></small>
   </footer>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+  <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
 </body>
 </html>
