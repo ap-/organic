3205b013bc8c5de819a38423c3495cee5975ae4c
ap- <github@ameso.com.au>
2012-02-22 09:05:22 +1100

    clean header

--- header.php
+++ header.php
@@ -13,34 +13,5 @@
  <header role="banner" class="hidden">
   <h1><?php echo get_site_option('site_name');?></h1>
  </header>
-<<<<<<< HEAD
- <nav role="navigation">
-  <div class="navbar navbar-fixed-top">
-   <div class="navbar-inner">
-    <div class="container-fluid">
-     <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-     </a>
-     <a class="brand" href="#"><?php echo get_site_option('site_name');?></a>
-     <div class="nav-collapse">
-      <ul class="nav">
-       <li class="active"><a href="#home">Home</a></li>
-       <li><a href="#about">About</a></li>
-       <li><a href="#contact">Contact</a></li>
-      </ul>
-      <p class="navbar-text pull-right">
-       <?php if ( is_user_logged_in() ) 
-        {global $current_user;get_currentuserinfo();echo '<a href="/wp-admin/">' . $current_user->display_name . '</a>';}
-       ?>
-      </p>
-     </div>
-    </div>
-   </div>
-  </div>
- </nav>
-=======
  <?php get_template_part('navigation');?>
->>>>>>> separate navigation into own file
 <div class="container-fluid">
