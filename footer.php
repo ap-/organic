39b16fb5bce42e27a343f06a0c5eea48312c160c
ap- <github@ameso.com.au>
2012-02-22 00:21:11 +1100

    create dynamic copyright footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
   <footer role="contentinfo">
-      <small><p>&copy; Organic 2012</p></small>
+      <small><p>© 2012 <?php echo esc_html( get_site_option( 'site_name' ) ); ?></p></small>
   </footer>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
   <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
