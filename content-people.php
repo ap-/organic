b87ebac6d928eacaede5b0aac7d98578e19d1d7e
ap- <github@ameso.com.au>
2012-02-23 11:15:16 +1100

    Update content-people.php

--- content-people.php
+++ content-people.php
@@ -16,12 +16,24 @@
 </div>
 
 <div class="row-fluid">
+
+<div class="span12">
+ <a class="btn" href="/" title = "All">All</a>
+ <?php 
+  $cat = get_query_var('cat');
+  foreach(get_categories('parent=0') as $category)
+  {echo '<a class="btn" href="../' . $category->slug . '/" title="' .  $category->name  . '" ' . '>' . $category->name.'</a> ';}
+ ?>
+</div>
+
+<hr>
+
 <ul class="thumbnails span12">
 <?php if ( have_posts() ) : ?>
  <?php while ( have_posts() ) : the_post(); ?>
   <li>
    <a href="<?php the_permalink()?>" class="thumbnail shadow">
-    <?php if ( has_post_thumbnail() ) { ?><?php the_post_thumbnail('thumbnail',array('class' => ''));?><?php } else { ?><img src="http://placehold.it/150x150" alt=""><?php } ?>       
+    <?php if ( has_post_thumbnail() ) { ?><?php the_post_thumbnail('thumbnail',array('class' => ''));?><?php } else { ?><img src="http://placehold.it/150x150" alt=""><?php } ?>         
     <p><?php the_title();?></p> 
   </a>
   </li>     
