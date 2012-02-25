501e1120234b9e57ddd87b613e5f0c451a23f5ff
ap- <github@ameso.com.au>
2012-02-25 15:15:33 +1100

    Update sidebar-people.php

--- sidebar-people.php
+++ sidebar-people.php
@@ -8,18 +8,18 @@ $categories=get_categories($cat_args);
     $args=array('category__in' => array($category->term_id),'caller_get_posts'=>1,'showposts'=>100,'orderby'=>'title','order'=>'asc','numberposts' => 100);
     $posts=get_posts($args);
       if ($posts) {
-        echo '<div class="accordion-group"><ul class="nav nav-list"><div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-categories" href="#collapse-' . $category->slug . '">' . $category->name.'</a></div>';
-        echo '<div id="collapse-' . $category->slug . '" class="accordion-body collapse in"><div class="accordion-inner">';
+        echo '<div class="accordion-group"><div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-categories" href="#collapse-' . $category->slug . '">' . $category->name.'</a></div>';
+        echo '<div id="collapse-' . $category->slug . '" class="accordion-body collapse in">';
+        echo '<div class="sidebar-nav">';
+        echo '<ul class=" accordion-inner nav nav-list">';
         foreach($posts as $post) {
           setup_postdata($post); ?>
-          <li <?php if ($activepost==$post->ID) {echo 'class="active"';}?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+          <li<?php if ($activepost==$post->ID) {echo ' class="active"';}?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
           <?php
         } // foreach($posts
       } // if ($posts
-    echo '</div></div></ul></div>';
+    echo '</ul></div></div></div>';
     } // foreach($categories
 ?>
 </div><!--accordion-->
 </div>
-
-
