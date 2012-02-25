5606802a7354a5a5fb32a32a3e87c178bbc55486
ap- <github@ameso.com.au>
2012-02-25 15:53:37 +1100

    Update sidebar-people.php

--- sidebar-people.php
+++ sidebar-people.php
@@ -1,15 +1,17 @@
 <div class="span3">
-<div class="accordion" id="accordion-categories">
+<div class="accordion shadow" id="accordion-categories">
 <?php
 $activepost=$post->ID;
+$postcategory=get_the_category();
 $cat_args=array('orderby'=>'name','order' => 'ASC');
 $categories=get_categories($cat_args);
   foreach($categories as $category) { 
     $args=array('category__in' => array($category->term_id),'caller_get_posts'=>1,'showposts'=>100,'orderby'=>'title','order'=>'asc','numberposts' => 100);
     $posts=get_posts($args);
       if ($posts) {
+        if ($postcategory[0]->category_nicename==$category->slug) {$customstate = 'in';} else {$customstate = 'out';};
         echo '<div class="accordion-group"><div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-categories" href="#collapse-' . $category->slug . '">' . $category->name.'</a></div>';
-        echo '<div id="collapse-' . $category->slug . '" class="accordion-body collapse in">';
+        echo '<div id="collapse-' . $category->slug . '" class="accordion-body collapse ' . $customstate . '">';
         echo '<div class="sidebar-nav">';
         echo '<ul class=" accordion-inner nav nav-list">';
         foreach($posts as $post) {
