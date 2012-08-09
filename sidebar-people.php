bff6a239925b9382a9e3a4238c3a9e10e5961459
ap- <github@ameso.com.au>
2012-08-09 19:29:14 +1000

    Update sidebar-people.php

--- sidebar-people.php
+++ sidebar-people.php
@@ -9,7 +9,7 @@ $categories=get_categories($cat_args);
     $posts=get_posts($args);
       if ($posts) {
         if ($postcategory[0]->category_nicename==$category->slug) {$customstate = 'in';} else {$customstate = 'out';};
-        echo '<div class="accordion-group shadow"><div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-categories" href="#collapse-' . $category->slug . '">' . $category->name.'</a></div>';
+        echo '<div class="accordion-group shadow"><div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion-categories" href="#collapse-' . $category->slug . '">' . $category->name.'<div class="pull-right"><i class="icon-list-alt"></i></div></a></div>';
         echo '<div id="collapse-' . $category->slug . '" class="accordion-body collapse ' . $customstate . '">';
         echo '<div class="sidebar-nav">';
         echo '<ul class=" accordion-inner nav nav-list">';
