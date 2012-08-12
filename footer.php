f23727afbe472bfaea115bb6ffa2eaba8146f09f
ap- <github@ameso.com.au>
2012-08-12 11:44:37 +1000

    simplify to vanilla masonry

--- footer.php
+++ footer.php
@@ -5,16 +5,11 @@
 </div><!--/container-->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
-<script src="<?php echo get_bloginfo('template_directory'); ?>/assets/js/jquery.masonry.min.js"></script>
-<script src="<?php echo get_bloginfo('template_directory'); ?>/assets/js/jquery.imagesloaded.min.js"></script>
+<script src="<?php echo get_bloginfo('template_directory'); ?>/assets/js/masonry.min.js"></script>
 <script>
-var $container = $('#iMasonary');
-$container.imagesLoaded(function(){
-  $container.masonry({
-    itemSelector : '.iMasonaryItems'
-  });
-});
+window.onload = function() {
+  var wall = new Masonry( document.getElementById('iMasonry') );
+};
 </script>
-
 </body>
 </html>
