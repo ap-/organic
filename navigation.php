b120bfb17c4f7dfaa338ca7f905cdf400af9427a
ap- <github@ameso.com.au>
2014-08-23 17:41:48 +1000

    add custom.css, fix navigation.php

--- navigation.php
+++ navigation.php
@@ -1,26 +1,33 @@
- <nav role="navigation" class="navbar navbar-fixed-top">
-   <div class="navbar-inner">
-    <div class="container-fluid">
-     <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-     </a>
-     <a class="brand" href="/"><?php echo get_site_option('site_name');?></a>
-     <div class="nav-collapse">
-      <ul class="nav">
+<nav role="navigation" class="navbar navbar-default navbar-fixed-top">
+   <div class="container-fluid">
+    <div class="navbar-header">
+     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+     </button>
+      <li><a class="brand" href="/"><?php echo get_site_option('site_name');?></a></li>
+    </div>
+
+
+     <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
        <li class="active"><a href="/">Home</a></li>
        <li><a href="/people/">People</a></li>
        <li><a href="/books/">Books</a></li>
        <li><a href="/additives/">Additives</a></li>
       </ul>
-      <p class="navbar-text pull-right">
-       <?php if ( is_user_logged_in() ) 
-        {global $current_user;get_currentuserinfo();echo '<a href="/wp-admin/">' . $current_user->display_name . '</a>';}
-       ?>
-      </p>
+
+      <ul class="nav navbar-nav navbar-right">
+       <li>
+        <?php if ( is_user_logged_in() ) 
+         {global $current_user;get_currentuserinfo();echo '<a href="/wp-admin/">' . $current_user->display_name . '</a>';}
+        ?>
+       </li>
+      </ul>
+
      </div>
-    </div>
    </div>
  </nav>
  
